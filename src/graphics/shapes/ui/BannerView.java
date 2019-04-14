638c15595058ae99c9863d3dbec082c91b00697e
Nicolas Herr <nicolas.herr2@etu.unistra.fr>
2019-04-14 15:40:33 +0200

    Button Color with Icon

--- src/graphics/shapes/ui/BannerView.java
+++ src/graphics/shapes/ui/BannerView.java
@@ -1,31 +1,64 @@
 package graphics.shapes.ui;
 
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import graphics.shapes.ui.component.ButtonColor;
 import graphics.ui.Controller;
 import graphics.ui.View;
 
 public class BannerView extends View {
 
+	private ClassLoader classLoader;
+	private ButtonColor btnc;
+	private ButtonColor btnc2;
+	
+
+	private JPanel p1;
+	private JPanel p2;
+	private JPanel p3;
+	
+	
 	public BannerView(Object model) {
 		super(model);
-		this.setInterface();
+		classLoader = Thread.currentThread().getContextClassLoader();
+		try {
+			this.setInterface();
+		} catch (Exception e) {
+
+		}
 	}
 	
-	private void setInterface() {
-		JPanel p1 = new JPanel();
-		JPanel p2 = new JPanel();
-		JPanel p3 = new JPanel();
-		this.add(p1); // selection pivot ...
-		this.add(p2); // creation de shape
-		this.add(p3); // modif color
-		p1.add(new JButton("Selection click"));
-		p1.add(new JButton("Selection drag"));
-		p1.add(new JButton("Pivot"));
-		p2.add(new JButton("triangle"));
-		p2.add(new JButton("rectangle"));
-		p2.add(new JButton("circle"));
-		p3.add(new JButton("Color"));
+	private void setInterface() throws Exception {
+		
+		this.p1 = new JPanel();
+		this.p2 = new JPanel();
+		this.p3 = new JPanel();
+		
+		this.add(this.p1); // selection pivot ...
+		
+		this.p1.add(new JButton("Selection drag"));
+		this.p1.add(new JButton("Pivot"));
+		
+		this.add(this.p2); // creation de shape
+		
+		this.p2.add(new JButton("triangle"));
+		this.p2.add(new JButton("rectangle"));
+		this.p2.add(new JButton("circle"));
+		
+		this.add(this.p3); // modif color
+		
+		this.btnc = new ButtonColor(getIcon("fill.png"),((BannerController)this.getController()).doPrint());
+		this.btnc2 = new ButtonColor(getIcon("bounds.jpg"),((BannerController)this.getController()).doPrint());
+	
+		this.p3.add(this.btnc);
+		this.p3.add(this.btnc2);
+		
+		
 	}
 	
 	@Override
@@ -33,4 +66,18 @@ public class BannerView extends View {
 		return new BannerController(model);
 	}
 	
+	
+	public ButtonColor getFillBtn() {
+		return this.btnc;
+	}
+
+	public ButtonColor getStrokeBtn() {
+		return this.btnc2;
+	}
+	
+	private ImageIcon getIcon(String s) throws Exception {
+		
+		InputStream input = this.classLoader.getResourceAsStream(s);
+		return new ImageIcon(ImageIO.read(input));
+	}
 }
---
+++ src/graphics/shapes/ui/component/ButtonColor.java
@@ -0,0 +1,73 @@
+package graphics.shapes.ui.component;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class ButtonColor extends JButton {
+	
+	
+	private static final Color DEFAULT_COLOR = Color.LIGHT_GRAY;
+	private static final int SPACE5 = 5;
+	private static final int SPACE10 = 10;
+	private static final int SPACE20 = 20;
+	private static final int DEFAULTPOS = 0;
+
+	private Color current;
+	
+	public ButtonColor(Icon icon,ActionListener c) {
+		super(icon);
+		this.current = DEFAULT_COLOR;
+		init(c);
+	}
+	
+	private void init(ActionListener c) {
+		this.setPreferredSize(new Dimension(SPACE20*2,this.getPreferredSize().height+SPACE20));
+		this.addActionListener(c);
+		this.cursor();
+		
+	}
+	
+	private void cursor() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		this.setCursor(toolkit.createCustomCursor(((ImageIcon)this.getIcon()).getImage(), new Point(0,0), "test"));
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g.create();
+		g2.clearRect(DEFAULTPOS, DEFAULTPOS,(int) this.getPreferredSize().getWidth(),(int) this.getPreferredSize().getHeight());
+	    g2.setColor(Color.black);
+	    g2.drawRect(DEFAULTPOS, DEFAULTPOS, getWidth(),getHeight());
+	    
+	    int centerX = this.getWidth()/2;
+	    
+	    int widthColorRectangle = this.getWidth()-(SPACE10); 
+    	int posXColorRectangle = centerX-(widthColorRectangle/2);
+    	int posYColorRectangle  = this.getHeight()-SPACE20;
+    	int heightColorRectangle = this.getWidth()-SPACE20-SPACE10;
+    	g2.setColor(current);
+    	g2.fillRect(posXColorRectangle, posYColorRectangle, widthColorRectangle, heightColorRectangle );
+	    
+	    if (this.getIcon()!=null) {
+	    	this.getIcon().paintIcon(this, g2, centerX-(this.getIcon().getIconWidth()/2), SPACE5);
+	    }
+	   
+	    g2.dispose();
+	}
+	
+	
+	public void setColor(Color c) {
+		this.current = c;
+	}
+	
+	
+}
